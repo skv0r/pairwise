91b74341fa8bec86795f4238feea466ca797047b
skv0r <skvorgrand@gmail.com>
2026-09-04 18:53:58 +0200

    feat: учебный массив коворкинг-зон

---
+++ app/data/spots.ts
@@ -0,0 +1,16 @@
+
+type SpotType = 
+    {
+        id: number,
+        name: string,
+        city: string,
+        adress: string,
+    }
+
+export const spots: SpotType[] = [
+    {
+        id: 0, 
+        name: "Точка Кипения",
+        city: "Санкт-Петербург", 
+        adress: "Большая Морская, д. 67, лит. А"
+    }]
